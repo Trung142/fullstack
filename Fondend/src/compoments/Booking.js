1cc12d7838c6cba445d44c7220c3e89c8ac0ea34
your name <nguonsong47@gmail.com>
2023-11-05 15:01:00 +0700

    barber_react

--- Fondend/src/compoments/Booking.js
+++ Fondend/src/compoments/Booking.js
@@ -1,7 +1,11 @@
+import "./Booking.scss";
 const Booking = (props) => {
     return (
         <div>
             Hello you!
+            <div>
+                tôi la một trong nhung nguoi
+            </div>
         </div>
     );
 }
